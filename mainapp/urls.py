98cfa3461c61e2e3a1643b1d14c92ad28d2c6fae
Beesolars <beesolarswebsitemanage@gmail.com>
2024-12-02 00:30:19 +0530

    update

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('',views.homepage , name='home' ),
+    path('project/',views.project , name='project' ),
 
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -6,3 +6,5 @@ def homepage(request):
     return render(request,"tempsfiles/index.html")
 
 
+def project(request):
+    return render(request,"tempsfiles/projects.html")
